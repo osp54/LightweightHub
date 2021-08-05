8739dac3118468ce9e0eb8d4bc0470d7e81e7a49
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-05 15:59:34 +0300

    Update Config.java

--- src/main/java/inside/Config.java
+++ src/main/java/inside/Config.java
@@ -21,21 +21,6 @@ public class Config{
     /** If {@code true}, player connection logging will be enabled. */
     public boolean logConnects = false;
 
-    /** List with {@link EffectData} that are triggered when the event is called. */
-    public Map<String, EffectData> eventEffects = new HashMap<>();
-
-    {
-        eventEffects.put("move", new EffectData(0, 0, 30, 0, "#4169e1ff", "fire"));
-        eventEffects.put("leave", new EffectData(0, 0, 30, 0, "#4169e1ff", "plasticExplosion"));
-        eventEffects.put("join", new EffectData(0, 0, 30, 0, "#cbd97f", "instBomb"));
-    }
-
-    /** List with static {@link EffectData}. */
-    public List<EffectData> effects = Arrays.asList(
-            new EffectData(240, 240, 30, 2000, "#ff0000ff", "explosion"),
-            new EffectData(160, 160, 30, 3000, "#ff0000ff", "explosion")
-    );
-
     /** List with {@link HostData}. */
     public List<HostData> servers = Arrays.asList(
             new HostData(1000, 7, 22, 37, 200, 320, 200, 288),
