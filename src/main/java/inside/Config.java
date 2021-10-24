8dc7132e04dd980d0ead553950ac737fcd612710
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-10-24 20:39:56 +0300

    Шыза

--- src/main/java/inside/Config.java
+++ src/main/java/inside/Config.java
@@ -7,9 +7,9 @@ public class Config {
 
     /** List with {@link HostData}. */
     public List<HostData> servers = Arrays.asList(
-            new HostData(1000, 7, 22, 37, 200, 320, 200, 288),
-            new HostData(2000, 7, 7, 22, 80, 200, 80, 168),
-            new HostData(3000, 7, 22, 7, 200, 80, 200, 48),
-            new HostData(4000, 7, 37, 23, 320, 200, 320, 168)
+            new HostData(1000, 7, 22, 37, 200, 320),
+            new HostData(2000, 7, 7, 22, 80, 200),
+            new HostData(3000, 7, 22, 7, 200, 80),
+            new HostData(4000, 7, 37, 23, 320, 200)
     );
 }
