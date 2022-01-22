c090bbd9f3ca28b3482758278af84a900462718a
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-22 16:07:46 +0300

    Update Config.java

--- src/main/java/inside/Config.java
+++ src/main/java/inside/Config.java
@@ -4,10 +4,5 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Config {
-    public List<HostData> servers = Arrays.asList(
-            new HostData(1000, 7, 22, 37, 200, 320, 200, 288),
-            new HostData(2000, 7, 7, 22, 80, 200, 80, 168),
-            new HostData(3000, 7, 22, 7, 200, 80, 200, 48),
-            new HostData(4000, 7, 37, 23, 320, 200, 320, 168)
-    );
+    public List<HostData> servers = Arrays.asList();
 }
